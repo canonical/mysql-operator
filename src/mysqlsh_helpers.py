68faabe45f6b06d0aca1ebeb902084c7fc103264
Paulo Machado <paulo.machado@canonical.com>
2022-04-04 15:31:22 -0300

    Add mysqlsh wrapper (#3)
    * Add mysqlsh wrapper
    
    Inherited code from machine charm, plus
    * add mode param to choose from "sql" or "python" script type
    
    * fixing lint
    
    * Rm mode, fix docstrings, add type hint
    
    * Fixed docstrings

--- src/mysqlsh_helpers.py
+++ src/mysqlsh_helpers.py
@@ -5,6 +5,10 @@
 """Helper class to manage the MySQL InnoDB cluster lifecycle with MySQL Shell."""
 
 import logging
+import os
+import subprocess
+import tempfile
+from typing import AnyStr
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +22,31 @@ class MySQL:
 
     def __init__(self):
         pass
+
+    @property
+    def mysqlsh_bin(self) -> str:
+        """Determine binary path for MySQL Shell.
+
+        Returns:
+            Path to binary mysqlsh
+        """
+        # Allow for various versions of the mysql-shell snap
+        # When we get the alias use /snap/bin/mysqlsh
+        if os.path.exists("/snap/bin/mysqlsh"):
+            return "/snap/bin/mysqlsh"
+        if os.path.exists("/snap/bin/mysql-shell.mysqlsh"):
+            return "/snap/bin/mysql-shell.mysqlsh"
+        # Default to the full path version
+        return "/snap/bin/mysql-shell"
+
+    @property
+    def mysqlsh_common_dir(self) -> str:
+        """Determine snap common dir for mysqlsh.
+
+        Returns:
+            Path to common dir
+        """
+        return "/root/snap/mysql-shell/common"
 
     def configure_mysql_users(self):
         """Configure the MySQL users for the instance.
@@ -38,3 +67,32 @@ class MySQL:
     def add_instance_to_cluster(self):
         """Add an instance to the InnoDB cluster."""
         pass
+
+    def run_mysqlsh_script(self, script: str) -> AnyStr:
+        """Execute a MySQL shell script.
+
+        Raises CalledProcessError if the script gets a non-zero return code.
+
+        Args:
+            script: Mysqlsh script string
+
+        Returns:
+            Byte string subprocess output
+        """
+        if not os.path.exists(self.mysqlsh_common_dir):
+            # Pre-execute mysqlsh to create self.mysqlsh_common_dir
+            # If we don't do this the real execution will fail with an
+            # ambiguous error message. This will only ever execute once.
+            cmd = [self.mysqlsh_bin, "--help"]
+            subprocess.check_call(cmd, stderr=subprocess.PIPE)
+
+        # Use the self.mysqlsh_common_dir dir for the confined
+        # mysql-shell snap.
+        with tempfile.NamedTemporaryFile(mode="w", dir=self.mysqlsh_common_dir) as _file:
+            _file.write(script)
+            _file.flush()
+
+            # Specify python as this is not the default in the deb version
+            # of the mysql-shell snap
+            cmd = [self.mysqlsh_bin, "--no-wizard", "--python", "-f", _file.name]
+            return subprocess.check_output(cmd, stderr=subprocess.PIPE)
