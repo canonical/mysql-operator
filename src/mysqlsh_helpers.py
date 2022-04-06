2efbc1e6563fb3458fc9728ba72e516ebaff404d
shayancanonical <99665202+shayancanonical@users.noreply.github.com>
2022-04-06 10:24:05 -0400

    DPE-145 Implement a helper method to configure instance for InnoDB (#4)
    * Implement a helper method to configure instance for InnoDB
    
    * Address PR feedback
    
    * Slightly improve configure instance tests
    
    * Add missing docstring
    
    * Fix lint warnings
    
    * Add missing return type for method

--- src/mysqlsh_helpers.py
+++ src/mysqlsh_helpers.py
@@ -8,9 +8,16 @@ import logging
 import os
 import subprocess
 import tempfile
-from typing import AnyStr
+
+from tenacity import retry, stop_after_delay, wait_fixed
 
 logger = logging.getLogger(__name__)
+
+
+class MySQLInstanceConfigureError(Exception):
+    """Exception raised when there is an issue configuring a MySQL instance."""
+
+    pass
 
 
 class MySQLConfigureMySQLUsersError(Exception):
@@ -36,10 +43,10 @@ class MySQL:
         """Initialize the MySQL class.
 
         Args:
-            root_password: Password for the 'root' user
-            cluster_admin_user: User name for the cluster admin user
-            cluster_admin_password: Password for the cluster admin user
-            instance_address: Address of the targeted instance
+            root_password: password for the 'root' user
+            cluster_admin_user: user name for the cluster admin user
+            cluster_admin_password: password for the cluster admin user
+            instance_address: address of the targeted instance
         """
         self.root_password = root_password
         self.cluster_admin_user = cluster_admin_user
@@ -100,19 +107,45 @@ class MySQL:
             logger.exception(f"Failed to configure users for: {self.instance_address}", exc_info=e)
             raise MySQLConfigureMySQLUsersError(e.stdout)
 
-    def configure_instance(self):
+    def configure_instance(self) -> None:
         """Configure the instance to be used in an InnoDB cluster."""
-        pass
+        commands = (
+            f"dba.configure_instance('{self.cluster_admin_user}:{self.cluster_admin_password}@{self.instance_address}')",
+            f"my_shell = shell.connect('{self.cluster_admin_user}:{self.cluster_admin_password}@{self.instance_address}')",
+            'my_shell.run_sql("RESTART;");',
+        )
 
-    def create_cluster(self):
+        try:
+            logger.debug("Configuring instance for InnoDB")
+            self._run_mysqlsh_script("\n".join(commands))
+
+            logger.debug("Waiting until MySQL is restarted")
+            self._wait_until_mysql_connection()
+        except subprocess.CalledProcessError as e:
+            logger.exception(f"Failed to configure instance: {self.instance_address}", exc_info=e)
+            raise MySQLInstanceConfigureError(e.stderr)
+
+    def create_cluster(self) -> None:
         """Create an InnoDB cluster with Group Replication enabled."""
         pass
 
-    def add_instance_to_cluster(self):
+    def add_instance_to_cluster(self) -> None:
         """Add an instance to the InnoDB cluster."""
         pass
 
-    def _run_mysqlsh_script(self, script: str) -> AnyStr:
+    @retry(reraise=True, stop=stop_after_delay(30), wait=wait_fixed(5))
+    def _wait_until_mysql_connection(self) -> None:
+        """Wait until a connection to MySQL has been obtained.
+
+        Retry every 5 seconds for 30 seconds if there is an issue obtaining a connection.
+        """
+        commands = (
+            f"my_shell = shell.connect('{self.cluster_admin_user}:{self.cluster_admin_password}@{self.instance_address}')",
+        )
+
+        self._run_mysqlsh_script("\n".join(commands))
+
+    def _run_mysqlsh_script(self, script: str) -> None:
         """Execute a MySQL shell script.
 
         Raises CalledProcessError if the script gets a non-zero return code.
@@ -139,9 +172,9 @@ class MySQL:
             # Specify python as this is not the default in the deb version
             # of the mysql-shell snap
             cmd = [self.mysqlsh_bin, "--no-wizard", "--python", "-f", _file.name]
-            return subprocess.check_output(cmd, stderr=subprocess.PIPE)
+            subprocess.check_output(cmd, stderr=subprocess.PIPE)
 
-    def _run_mysqlcli_script(self, script: str) -> AnyStr:
+    def _run_mysqlcli_script(self, script: str) -> None:
         """Execute a MySQL CLI script.
 
         Execute SQL script as instance root user.
@@ -162,4 +195,4 @@ class MySQL:
             "-e",
             script,
         ]
-        return subprocess.check_output(cmd, stderr=subprocess.PIPE)
+        subprocess.check_output(cmd, stderr=subprocess.PIPE)
--- tests/unit/test_mysqlsh_helpers.py
+++ tests/unit/test_mysqlsh_helpers.py
@@ -5,10 +5,24 @@ import subprocess
 import unittest
 from unittest.mock import patch
 
-from mysqlsh_helpers import MySQL, MySQLConfigureMySQLUsersError
+from mysqlsh_helpers import (
+    MySQL,
+    MySQLConfigureMySQLUsersError,
+    MySQLInstanceConfigureError,
+)
 
 
 class TestMySQL(unittest.TestCase):
+    def setUp(self):
+        root_password = "password"
+        cluster_admin_user = "clusteradmin"
+        cluster_admin_password = "innodb"
+        instance_address = "127.0.0.1"
+
+        self.mysql = MySQL(
+            root_password, cluster_admin_user, cluster_admin_password, instance_address
+        )
+
     @patch("mysqlsh_helpers.MySQL._run_mysqlcli_script")
     def test_configure_mysql_users(self, _run_mysqlcli_script):
         """Test failed to configuring the MySQL users."""
@@ -63,3 +77,46 @@ class TestMySQL(unittest.TestCase):
         self.assertEqual(_m.mysqlsh_common_dir, "/root/snap/mysql-shell/common")
         _exists.return_value = False
         self.assertEqual(_m.mysqlsh_common_dir, "/tmp")
+
+    @patch("mysqlsh_helpers.MySQL._run_mysqlsh_script")
+    @patch("mysqlsh_helpers.MySQL._wait_until_mysql_connection")
+    def test_configure_instance(self, _wait_until_mysql_connection, _run_mysqlsh_script):
+        """Test a successful execution of configure_instance."""
+        configure_instance_commands = (
+            "dba.configure_instance('clusteradmin:innodb@127.0.0.1')",
+            "my_shell = shell.connect('clusteradmin:innodb@127.0.0.1')",
+            'my_shell.run_sql("RESTART;");',
+        )
+
+        self.mysql.configure_instance()
+
+        _run_mysqlsh_script.assert_called_once_with("\n".join(configure_instance_commands))
+        _wait_until_mysql_connection.assert_called_once()
+
+    @patch("mysqlsh_helpers.MySQL._run_mysqlsh_script")
+    @patch("mysqlsh_helpers.MySQL._wait_until_mysql_connection")
+    def test_configure_instance_exceptions(
+        self, _wait_until_mysql_connection, _run_mysqlsh_script
+    ):
+        """Test exceptions raised by methods called in configure_instance."""
+        # Test an issue with _run_mysqlsh_script
+        _run_mysqlsh_script.side_effect = subprocess.CalledProcessError(cmd="mock", returncode=127)
+
+        with self.assertRaises(MySQLInstanceConfigureError):
+            self.mysql.configure_instance()
+
+        _wait_until_mysql_connection.assert_not_called()
+
+        # Reset mocks
+        _run_mysqlsh_script.reset_mock()
+        _wait_until_mysql_connection.reset_mock()
+
+        # Test an issue with _wait_until_mysql_connection
+        _wait_until_mysql_connection.side_effect = subprocess.CalledProcessError(
+            cmd="mock", returncode=127
+        )
+
+        with self.assertRaises(MySQLInstanceConfigureError):
+            self.mysql.configure_instance()
+
+        _run_mysqlsh_script.assert_called_once()
