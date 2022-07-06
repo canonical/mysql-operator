ca5fce2a763f7195ce2e6610e925836c4be8f498
Paulo Machado <paulo.machado@canonical.com>
2022-07-06 15:08:30 -0300

    more relation attributes, user naming convention, user removal

--- src/relations/database.py
+++ src/relations/database.py
@@ -13,7 +13,7 @@ from charms.data_platform_libs.v0.database_provides import (
 from charms.mysql.v0.mysql import (
     MySQLClientError,
     MySQLCreateApplicationDatabaseAndScopedUserError,
-    MySQLDeleteUsersForUnitError,
+    MySQLDeleteUserForRelationError,
 )
 from ops.charm import RelationDepartedEvent
 from ops.framework import Object
@@ -26,6 +26,8 @@ logger = logging.getLogger(__name__)
 
 
 class DatabaseRelation(Object):
+    """Standard database relation class."""
+
     def __init__(self, charm):
         super().__init__(charm, "database")
 
@@ -35,7 +37,7 @@ class DatabaseRelation(Object):
         self.framework.observe(self.database.on.database_requested, self._on_database_requested)
 
         self.framework.observe(
-            self._charm.on["database"].relation_departed, self._on_database_departed
+            self._charm.on["database"].relation_broken, self._on_database_broken
         )
 
     def _get_or_set_password(self, relation) -> str:
@@ -47,35 +49,43 @@ class DatabaseRelation(Object):
         Returns:
             str: The password.
         """
-        if password := relation.data[self._charm.app].get(f"password"):
+        if password := relation.data[self._charm.app].get("password"):
             return password
         password = generate_random_password(PASSWORD_LENGTH)
-        relation.data[self._charm.app][f"password"] = password
+        relation.data[self._charm.app]["password"] = password
         return password
 
     def _on_database_requested(self, event: DatabaseRequestedEvent):
         """Handle the `database-requested` event."""
-
         if not self._charm.unit.is_leader():
             return
 
+        # get base relation data
         db_name = event.database
-        db_user = event.app.name
+        db_user = f"relation-{event.relation.id}"
         db_pass = self._get_or_set_password(event.relation)
+        db_version = self._charm._mysql.get_mysql_version()
 
-        remote_host = event.relation.data[event.unit].get("private-address")
-        remote_unit = event.unit.name
+        remote_app = event.app.name
 
         try:
             self._charm._mysql.create_application_database_and_scoped_user(
-                db_name, db_user, db_pass, remote_host, remote_unit
+                db_name, db_user, db_pass, "%", remote_app
             )
-            uri = self._charm._mysql.get_cluster_primary_address()
+            primary_endpoint = self._charm._mysql.get_cluster_primary_address()
             self.database.set_credentials(event.relation.id, db_user, db_pass)
-            self.database.set_endpoints(event.relation.id, uri)
-            logger.info(f"Created user for unit {remote_unit}")
+            self.database.set_endpoints(event.relation.id, primary_endpoint)
+            self.database.set_version(event.relation.id, db_version)
+            secondaries_endpoints = self._charm._mysql.get_cluster_members_addresses() - set(
+                primary_endpoint
+            )
+            self.database.set_secondaries_endpoints(event.relation.id, secondaries_endpoints)
+            # TODO:
+            # add setup of tls, tls_ca and status
+
+            logger.info(f"Created user for app {remote_app}")
         except MySQLCreateApplicationDatabaseAndScopedUserError:
-            logger.error(f"Failed to create scoped user for unit {remote_unit}")
+            logger.error(f"Failed to create scoped user for app {remote_app}")
             self._charm.unit.status = BlockedStatus("Failed to create scoped user")
             return
         except MySQLClientError:
@@ -83,24 +93,23 @@ class DatabaseRelation(Object):
             self._charm.unit.status = BlockedStatus("Failed to retrieve endpoint")
             return
 
-    def _on_database_departed(self, event: RelationDepartedEvent) -> None:
-        """Handle the departure of legacy shared_db relation.
+    def _on_database_broken(self, event: RelationDepartedEvent) -> None:
+        """Handle the removal of database relation.
 
-        Remove unit name from allowed_units key.
+        Remove user, keeping database intact.
         """
         if not self._charm.unit.is_leader():
+            # run once by the leader
             return
 
         if event.departing_unit.app == self._charm.app:
             # Just run for departing of remote units
             return
 
-        departing_unit = event.departing_unit.name
-
-        # remove unit users
         try:
-            self._charm._mysql.delete_users_for_unit(departing_unit)
-            logger.info(f"Removed user for unit {departing_unit}")
-        except MySQLDeleteUsersForUnitError:
-            logger.error(f"Failed to delete users for unit {departing_unit}")
+            relation_id = event.relation.id
+            self._charm._mysql.delete_user_for_relation(relation_id)
+            logger.info(f"Removed user for relation {relation_id}")
+        except MySQLDeleteUserForRelationError:
+            logger.error(f"Failed to delete user for relation {relation_id}")
             return
