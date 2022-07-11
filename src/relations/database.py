a414c1b7af318a2ccbc36501e735ad8d40aac386
Paulo Machado <paulo.machado@canonical.com>
2022-07-11 17:51:33 -0300

    s/_charm/charm

--- src/relations/database.py
+++ src/relations/database.py
@@ -19,7 +19,7 @@ from ops.charm import RelationDepartedEvent
 from ops.framework import Object
 from ops.model import BlockedStatus
 
-from constants import PASSWORD_LENGTH
+from constants import DB_RELATION_NAME, PASSWORD_LENGTH
 from utils import generate_random_password
 
 logger = logging.getLogger(__name__)
@@ -29,15 +29,15 @@ class DatabaseRelation(Object):
     """Standard database relation class."""
 
     def __init__(self, charm):
-        super().__init__(charm, "database")
+        super().__init__(charm, DB_RELATION_NAME)
 
-        self._charm = charm
+        self.charm = charm
 
-        self.database = DatabaseProvides(self._charm, relation_name="database")
+        self.database = DatabaseProvides(self.charm, relation_name=DB_RELATION_NAME)
         self.framework.observe(self.database.on.database_requested, self._on_database_requested)
 
         self.framework.observe(
-            self._charm.on["database"].relation_broken, self._on_database_broken
+            self.charm.on[DB_RELATION_NAME].relation_broken, self._on_database_broken
         )
 
     def _get_or_set_password(self, relation) -> str:
@@ -49,51 +49,54 @@ class DatabaseRelation(Object):
         Returns:
             str: The password.
         """
-        if password := relation.data[self._charm.app].get("password"):
+        if password := relation.data[self.charm.app].get("password"):
             return password
         password = generate_random_password(PASSWORD_LENGTH)
-        relation.data[self._charm.app]["password"] = password
+        relation.data[self.charm.app]["password"] = password
         return password
 
     def _on_database_requested(self, event: DatabaseRequestedEvent):
         """Handle the `database-requested` event."""
-        if not self._charm.unit.is_leader():
+        if not self.charm.unit.is_leader():
+
             return
 
         # get base relation data
         relation_id = event.relation.id
         db_name = event.database
+        # user name is derived from the relation id
         db_user = f"relation-{relation_id}"
         db_pass = self._get_or_set_password(event.relation)
-        db_version = self._charm._mysql.get_mysql_version()
+        db_version = self.charm._mysql.get_mysql_version()
 
         remote_app = event.app.name
 
         try:
-            primary_endpoint = self._charm._mysql.get_cluster_primary_address()
+            primary_endpoint = self.charm._mysql.get_cluster_primary_address()
             self.database.set_credentials(relation_id, db_user, db_pass)
             self.database.set_endpoints(relation_id, primary_endpoint)
             self.database.set_version(relation_id, db_version)
             # get read only endpoints by removing primary from all members
-            read_only_endpoints = self._charm._mysql.get_cluster_members_addresses() - {
+            read_only_endpoints = self.charm._mysql.get_cluster_members_addresses() - {
                 primary_endpoint,
             }
 
             self.database.set_read_only_endpoints(relation_id, ",".join(read_only_endpoints))
             # TODO:
             # add setup of tls, tls_ca and status
-            self._charm._mysql.create_application_database_and_scoped_user(
+            # add extra roles parsing from relation data
+            self.charm._mysql.create_application_database_and_scoped_user(
                 db_name, db_user, db_pass, "%", remote_app
             )
 
             logger.info(f"Created user for app {remote_app}")
         except MySQLCreateApplicationDatabaseAndScopedUserError:
             logger.error(f"Failed to create scoped user for app {remote_app}")
-            self._charm.unit.status = BlockedStatus("Failed to create scoped user")
+            self.charm.unit.status = BlockedStatus("Failed to create scoped user")
             return
         except MySQLClientError as e:
             logger.exception("Failed to retrieve cluster info", exc_info=e)
-            self._charm.unit.status = BlockedStatus("Failed to retrieve cluster info")
+            self.charm.unit.status = BlockedStatus("Failed to retrieve cluster info")
             return
 
     def _on_database_broken(self, event: RelationDepartedEvent) -> None:
@@ -101,13 +104,13 @@ class DatabaseRelation(Object):
 
         Remove user, keeping database intact.
         """
-        if not self._charm.unit.is_leader():
+        if not self.charm.unit.is_leader():
             # run once by the leader
             return
 
         try:
             relation_id = event.relation.id
-            self._charm._mysql.delete_user_for_relation(relation_id)
+            self.charm._mysql.delete_user_for_relation(relation_id)
             logger.info(f"Removed user for relation {relation_id}")
         except MySQLDeleteUserForRelationError:
             logger.error(f"Failed to delete user for relation {relation_id}")
