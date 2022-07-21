fe16fc2d31a99b46e5c67ab84fb30e15c8d3d3ea
Paulo Machado <paulo.machado@canonical.com>
2022-07-21 14:34:11 -0300

    speeding up charm and database itest

--- tests/integration/test_database.py
+++ tests/integration/test_database.py
@@ -44,36 +44,37 @@ async def test_build_and_deploy(ops_test: OpsTest):
     await ops_test.model.deploy(app_charm, application_name=APPLICATION_APP_NAME, num_units=2)
 
     # Reduce the update_status frequency until the cluster is deployed
-    await ops_test.model.set_config({"update-status-hook-interval": "10s"})
+    with ops_test.fast_forward():
 
-    await ops_test.model.block_until(
-        lambda: len(ops_test.model.applications[DATABASE_APP_NAME].units) == 3
-    )
+        await ops_test.model.block_until(
+            lambda: len(ops_test.model.applications[DATABASE_APP_NAME].units) == 3
+        )
 
-    await ops_test.model.wait_for_idle(
-        apps=[DATABASE_APP_NAME],
-        status="active",
-        raise_on_blocked=True,
-        timeout=1000,
-    )
+        await ops_test.model.block_until(
+            lambda: len(ops_test.model.applications[APPLICATION_APP_NAME].units) == 2
+        )
+
+        asyncio.gather(
+            await ops_test.model.wait_for_idle(
+                apps=[DATABASE_APP_NAME],
+                status="active",
+                raise_on_blocked=True,
+                timeout=1000,
+            ),
+            await ops_test.model.wait_for_idle(
+                apps=[APPLICATION_APP_NAME],
+                status="waiting",
+                raise_on_blocked=True,
+                timeout=1000,
+            ),
+        )
+
     assert len(ops_test.model.applications[DATABASE_APP_NAME].units) == 3
 
     for unit in ops_test.model.applications[DATABASE_APP_NAME].units:
         assert unit.workload_status == "active"
 
-    await ops_test.model.block_until(
-        lambda: len(ops_test.model.applications[APPLICATION_APP_NAME].units) == 2
-    )
-
-    await ops_test.model.wait_for_idle(
-        apps=[APPLICATION_APP_NAME],
-        status="waiting",
-        raise_on_blocked=True,
-        timeout=1000,
-    )
     assert len(ops_test.model.applications[APPLICATION_APP_NAME].units) == 2
-
-    await ops_test.model.set_config({"update-status-hook-interval": "60m"})
 
 
 @pytest.mark.order(2)
@@ -83,11 +84,12 @@ async def test_relation_creation(ops_test: OpsTest):
     """Relate charms and wait for the expected changes in status."""
     await ops_test.model.relate(APPLICATION_APP_NAME, f"{DATABASE_APP_NAME}:database")
 
-    await ops_test.model.block_until(
-        lambda: is_relation_joined(ops_test, ENDPOINT, ENDPOINT) == True  # noqa: E712
-    )
+    with ops_test.fast_forward():
+        await ops_test.model.block_until(
+            lambda: is_relation_joined(ops_test, ENDPOINT, ENDPOINT) == True  # noqa: E712
+        )
 
-    await ops_test.model.wait_for_idle(apps=APPS, status="active")
+        await ops_test.model.wait_for_idle(apps=APPS, status="active")
 
 
 @pytest.mark.order(3)
@@ -103,11 +105,12 @@ async def test_relation_broken(ops_test: OpsTest):
         lambda: is_relation_broken(ops_test, ENDPOINT, ENDPOINT) == True  # noqa: E712
     )
 
-    await asyncio.gather(
-        ops_test.model.wait_for_idle(
-            apps=[DATABASE_APP_NAME], status="active", raise_on_blocked=True
-        ),
-        ops_test.model.wait_for_idle(
-            apps=[APPLICATION_APP_NAME], status="waiting", raise_on_blocked=True
-        ),
-    )
+    with ops_test.fast_forward():
+        await asyncio.gather(
+            ops_test.model.wait_for_idle(
+                apps=[DATABASE_APP_NAME], status="active", raise_on_blocked=True
+            ),
+            ops_test.model.wait_for_idle(
+                apps=[APPLICATION_APP_NAME], status="waiting", raise_on_blocked=True
+            ),
+        )
