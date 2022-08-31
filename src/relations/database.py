d94c168739f0873d841b0c62c8ebab68e304d05d
Paulo Machado <paulo.machado@canonical.com>
2022-08-30 22:01:46 -0300

    avoid removing user for a unit being destroyed

--- src/relations/database.py
+++ src/relations/database.py
@@ -17,7 +17,7 @@ from charms.mysql.v0.mysql import (
     MySQLGetClusterMembersAddressesError,
     MySQLGetMySQLVersionError,
 )
-from ops.charm import RelationDepartedEvent
+from ops.charm import RelationBrokenEvent
 from ops.framework import Object
 from ops.model import BlockedStatus
 
@@ -112,13 +112,19 @@ class DatabaseRelation(Object):
             logger.exception("Failed to get primary", exc_info=e)
             self.charm.unit.status = BlockedStatus("Failed to get primary")
 
-    def _on_database_broken(self, event: RelationDepartedEvent) -> None:
+    def _on_database_broken(self, event: RelationBrokenEvent) -> None:
         """Handle the removal of database relation.
 
         Remove user, keeping database intact.
         """
         if not self.charm.unit.is_leader():
             # run once by the leader
+            return
+
+        if len(self.charm._peers.units) == 0:
+            # safeguard against relation broken being triggered for
+            # a unit being torn down (instead of un-related)
+            # https://github.com/canonical/mysql-operator/issues/32
             return
 
         try:
