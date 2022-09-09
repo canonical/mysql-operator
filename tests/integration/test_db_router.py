34c28b5a2a27b960914b876465738d23b1c42a7a
Shayan Patel <shayan.patel@canonical.com>
2022-09-08 20:36:06 -0400

    Remove upterm session, and bump keystone units up to 2

--- tests/integration/test_db_router.py
+++ tests/integration/test_db_router.py
@@ -131,7 +131,7 @@ async def test_keystone_bundle_db_router(ops_test: OpsTest) -> None:
         "keystone",
         series="focal",
         application_name=KEYSTONE_APP_NAME,
-        num_units=1,
+        num_units=2,
     )
 
     # Deploy mysqlrouter and relate it to keystone
@@ -202,7 +202,7 @@ async def test_keystone_bundle_db_router(ops_test: OpsTest) -> None:
             "keystone",
             series="focal",
             application_name=ANOTHER_KEYSTONE_APP_NAME,
-            num_units=1,
+            num_units=2,
         )
 
         # Deploy mysqlrouter and relate it to keystone
