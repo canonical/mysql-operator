24d375b88218e43dffae3ab9d523d9c97ef01efb
Paulo Machado <paulo.machado@canonical.com>
2022-10-07 12:00:43 -0300

    ripped func from pg plus method extension

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -320,11 +320,12 @@ def is_relation_broken(ops_test: OpsTest, endpoint_one: str, endpoint_two: str) 
 
 
 @retry(stop=stop_after_attempt(8), wait=wait_fixed(15), reraise=True)
-def is_connection_possible(credentials: Dict) -> bool:
+def is_connection_possible(credentials: Dict, **extra_opts) -> bool:
     """Test a connection to a MySQL server.
 
     Args:
         credentials: A dictionary with the credentials to test
+        extra_opts: extra options for mysql connection
     """
     config = {
         "user": credentials["username"],
@@ -332,6 +333,7 @@ def is_connection_possible(credentials: Dict) -> bool:
         "host": credentials["host"],
         "raise_on_warnings": False,
         "connection_timeout": 10,
+        **extra_opts,
     }
 
     try:
@@ -770,3 +772,29 @@ async def write_random_chars_to_test_table(ops_test: OpsTest, primary_unit: Unit
     )
 
     return random_chars
+
+
+async def get_tls_ca(
+    ops_test: OpsTest,
+    unit_name: str,
+) -> str:
+    """Returns the TLS CA used by the unit.
+
+    Args:
+        ops_test: The ops test framework instance
+        unit_name: The name of the unit
+
+    Returns:
+        TLS CA or an empty string if there is no CA.
+    """
+    raw_data = (await ops_test.juju("show-unit", unit_name))[1]
+    if not raw_data:
+        raise ValueError(f"no unit info could be grabbed for {unit_name}")
+    data = yaml.safe_load(raw_data)
+    # Filter the data based on the relation name.
+    relation_data = [
+        v for v in data[unit_name]["relation-info"] if v["endpoint"] == "certificates"
+    ]
+    if len(relation_data) == 0:
+        return ""
+    return json.loads(relation_data[0]["application-data"]["certificates"])[0].get("ca")
