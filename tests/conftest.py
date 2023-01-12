66d841d83364bbef0e326744511d1a5e08b8ef48
Carl Csaposs <carl.csaposs@canonical.com>
2023-01-12 15:56:48 +0000

    Format

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+
 def pytest_addoption(parser):
     parser.addoption("--series", action="store", type=str, default="jammy")
 
