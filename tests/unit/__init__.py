dc02dec21cd462f786272f789ca018db5975f4bb
Carl Csaposs <carl.csaposs@canonical.com>
2023-01-27 13:24:33 +0000

    Delete __init__.py
    No longer needed in ops 2

--- tests/unit/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2022 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-import ops.testing
-
-# Since ops>=1.4 this enables better connection tracking.
-# See: More at https://juju.is/docs/sdk/testing#heading--simulate-can-connect
-ops.testing.SIMULATE_CAN_CONNECT = True
