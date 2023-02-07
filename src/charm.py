50460601eb836387b3a3b932ab585e1b85299a4d
Paulo Machado <paulo.machado@canonical.com>
2023-02-07 10:08:13 -0300

    remove ambiguity by validating event_unit_data

--- src/charm.py
+++ src/charm.py
@@ -211,16 +211,17 @@ class MySQLOperatorCharm(CharmBase):
             event.defer()
             return
 
-        # Defer if the instance is not configured for use in an InnoDB cluster
-        # Every instance gets configured for use in an InnoDB cluster on start
-        try:
-            event_unit_address = event.relation.data[event.unit]["private-address"]
-        except KeyError:
+        event_unit_data = event.relation.data.get(event.unit)
+        if not event_unit_data:
             # bypass when unit is no longer available
             logger.warning(f"Unit {event.unit.name} is no longer available")
             return
+
+        event_unit_address = event_unit_data["private-address"]
         event_unit_label = event.unit.name.replace("/", "-")
 
+        # Defer if the instance is not configured for use in an InnoDB cluster
+        # Every instance gets configured for use in an InnoDB cluster on start
         if not self._mysql.is_instance_configured_for_innodb(event_unit_address, event_unit_label):
             event.defer()
             return
