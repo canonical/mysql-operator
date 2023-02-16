7a75eedddc33b094d1453cef58dcd418fdb628c0
Shayan Patel <shayan.patel@canonical.com>
2023-02-16 14:24:19 -0500

    Update libpatch

--- lib/charms/mysql/v0/mysql.py
+++ lib/charms/mysql/v0/mysql.py
@@ -90,7 +90,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 13
+LIBPATCH = 14
 
 UNIT_TEARDOWN_LOCKNAME = "unit-teardown"
 
