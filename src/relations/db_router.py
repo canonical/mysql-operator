467ee9470bdf9986df756e1522b82ca4123944d0
Paulo Machado <paulo.machado@canonical.com>
2023-03-02 18:33:28 -0300

    chore, unrelated fix

--- src/relations/db_router.py
+++ src/relations/db_router.py
@@ -180,7 +180,7 @@ class DBRouterRelation(Object):
             return
 
         try:
-            primary_address = self._charm._mysql.get_cluster_primary_address().split(":")[0]
+            primary_address = self.charm._mysql.get_cluster_primary_address().split(":")[0]
         except MySQLGetClusterPrimaryAddressError:
             logger.error("Can't get primary address. Deferring")
             event.defer()
