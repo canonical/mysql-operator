62cd0fa7e020c9716a143cf391cc0bf3aa00e4e9
Paulo Machado <paulo.machado@canonical.com>
2023-03-03 11:27:49 -0300

    Fix early legacy relations handling (#137)
    * chore, unrelated fix
    
    * defer until unit initialization
    
    * replicated fix to other legacy relations, fix tests

--- tests/unit/test_relation_mysql_legacy.py
+++ tests/unit/test_relation_mysql_legacy.py
@@ -39,6 +39,7 @@ class TestMariaDBRelation(unittest.TestCase):
         # run start-up events to enable usage of the helper class
         self.harness.set_leader(True)
         self.charm.on.config_changed.emit()
+        self.charm.unit_peer_data["unit-initialized"] = "True"
         self.harness.update_config(
             {"mysql-interface-user": "mysql", "mysql-interface-database": "default_database"}
         )
