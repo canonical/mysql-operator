637adc012efb71646a567ffadb9fef307cea8c55
Shayan Patel <shayan.patel@canonical.com>
2023-03-06 20:35:23 -0500

    Clean up restore code (for a separate PR)

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -15,6 +15,11 @@ from ops.testing import Harness
 from tenacity import Retrying, stop_after_attempt
 
 from charm import MySQLOperatorCharm
+from mysql_vm_helpers import (
+    MySQLChangeSnapDaemonHomeDirectoryError,
+    MySQLCreateCustomMySQLDConfigError,
+    MySQLResetRootPasswordAndStartMySQLDError,
+)
 
 from .helpers import patch_network_get
 
@@ -128,8 +133,12 @@ class TestCharm(unittest.TestCase):
     @patch("mysql_vm_helpers.MySQL.initialize_juju_units_operations_table")
     @patch("mysql_vm_helpers.MySQL.create_cluster")
     @patch("mysql_vm_helpers.MySQL.reset_root_password_and_start_mysqld")
+    @patch("mysql_vm_helpers.MySQL.create_custom_mysqld_config")
+    @patch("mysql_vm_helpers.MySQL.change_snap_daemon_home_directory")
     def test_on_start(
         self,
+        _change_snap_daemon_home_directory,
+        _create_custom_mysqld_config,
         _reset_root_password_and_start_mysqld,
         _create_cluster,
         _initialize_juju_units_operations_table,
@@ -156,8 +165,12 @@ class TestCharm(unittest.TestCase):
     @patch("mysql_vm_helpers.MySQL.initialize_juju_units_operations_table")
     @patch("mysql_vm_helpers.MySQL.create_cluster")
     @patch("mysql_vm_helpers.MySQL.reset_root_password_and_start_mysqld")
+    @patch("mysql_vm_helpers.MySQL.create_custom_mysqld_config")
+    @patch("mysql_vm_helpers.MySQL.change_snap_daemon_home_directory")
     def test_on_start_exceptions(
         self,
+        _change_snap_daemon_home_directory,
+        _create_custom_mysqld_config,
         _reset_root_password_and_start_mysqld,
         _create_cluster,
         _initialize_juju_units_operations_table,
@@ -198,6 +211,26 @@ class TestCharm(unittest.TestCase):
 
         # test an exception with creating a cluster
         _create_cluster.side_effect = MySQLCreateClusterError
+
+        self.charm.on.start.emit()
+        self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
+
+        # test an exception with resetting the root password and starting mysqld
+        _reset_root_password_and_start_mysqld.side_effect = (
+            MySQLResetRootPasswordAndStartMySQLDError
+        )
+
+        self.charm.on.start.emit()
+        self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
+
+        # test an exception creating a custom mysqld config
+        _create_custom_mysqld_config.side_effect = MySQLCreateCustomMySQLDConfigError
+
+        self.charm.on.start.emit()
+        self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
+
+        # test an exception changing the snap_daemon user's home directory
+        _change_snap_daemon_home_directory.side_effect = MySQLChangeSnapDaemonHomeDirectoryError
 
         self.charm.on.start.emit()
         self.assertTrue(isinstance(self.harness.model.unit.status, BlockedStatus))
