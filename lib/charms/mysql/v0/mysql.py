d88132091903db71e898fe35c503852b2495567a
Paulo Machado <paulo.machado@canonical.com>
2023-04-04 11:11:07 -0300

    lib updates and version bump

--- lib/charms/mysql/v0/mysql.py
+++ lib/charms/mysql/v0/mysql.py
@@ -91,7 +91,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 19
+LIBPATCH = 20
 
 UNIT_TEARDOWN_LOCKNAME = "unit-teardown"
 
