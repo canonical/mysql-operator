9e5c911e3d6fd5188fffa696991a28e285a46663
Carl Csaposs <carl.csaposs@canonical.com>
2023-08-01 13:40:37 +0000

    Pin python dependencies with poetry (#192)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,10 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
-from argparse import ArgumentError
+
+import argparse
 
 
 def pytest_addoption(parser):
-    parser.addoption(
-        "--collect-groups",
-        action="store_true",
-        help="Collect test groups (used by GitHub Actions)",
-    )
-    parser.addoption("--group", type=int, help="Integration test group number")
     parser.addoption("--mysql-charm-series", help="Ubuntu series for mysql charm (e.g. jammy)")
     parser.addoption(
         "--mysql-charm-bases-index",
@@ -19,12 +14,10 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
-    if config.option.collect_groups:
-        config.option.collectonly = True
     if (config.option.mysql_charm_series is None) ^ (
         config.option.mysql_charm_bases_index is None
     ):
-        raise ArgumentError(
+        raise argparse.ArgumentError(
             None, "--mysql-charm-series and --mysql-charm-bases-index must be given together"
         )
     # Note: Update defaults whenever charmcraft.yaml is changed
