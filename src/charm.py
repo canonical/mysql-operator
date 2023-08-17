93b85dc9c0fa9dc4177e65c92913618b7c63f128
Paulo Machado <paulo.machado@canonical.com>
2023-08-17 11:09:26 -0300

    DPE-1800 in place upgrades (#292)
    * in-place upgrade support
    
    * fix test and use idle property
    
    * happy path working
    
    * messaging improvments
    
    * unit tests and better UI
    
    * happy path simple upgrade test
    
    * bump upgrade lib

--- src/charm.py
+++ src/charm.py
@@ -70,6 +70,7 @@ from mysql_vm_helpers import (
     MySQL,
     MySQLCreateCustomMySQLDConfigError,
     MySQLDataPurgeError,
+    MySQLInstallError,
     MySQLReconfigureError,
     MySQLResetRootPasswordAndStartMySQLDError,
     SnapServiceOperationError,
@@ -83,6 +84,7 @@ from relations.db_router import DBRouterRelation
 from relations.mysql import MySQLRelation
 from relations.mysql_provider import MySQLProvider
 from relations.shared_db import SharedDBRelation
+from upgrade import MySQLVMUpgrade, get_mysql_dependencies_model
 from utils import generate_random_hash, generate_random_password
 
 logger = logging.getLogger(__name__)
@@ -128,6 +130,12 @@ class MySQLOperatorCharm(MySQLCharmBase):
         self.s3_integrator = S3Requirer(self, S3_INTEGRATOR_RELATION_NAME)
         self.backups = MySQLBackups(self, self.s3_integrator)
         self.hostname_resolution = MySQLMachineHostnameResolution(self)
+        self.upgrade = MySQLVMUpgrade(
+            self,
+            dependency_model=get_mysql_dependencies_model(),
+            relation_name="upgrade",
+            substrate="vm",
+        )
 
     # =======================
     #  Charm Lifecycle Hooks
@@ -141,26 +149,10 @@ class MySQLOperatorCharm(MySQLCharmBase):
             self._reboot_on_detached_storage(event)
             return
 
-        # Initial setup operations like installing dependencies, and creating users and groups.
-        def set_retry_status(_):
-            self.unit.status = MaintenanceStatus(
-                "Failed to install and configure MySQL. Retrying..."
-            )
-
-        try:
-            for attempt in Retrying(
-                wait=wait_exponential(multiplier=10),
-                stop=stop_after_delay(60 * 5),
-                retry=tenacity.retry_if_exception_type(snap.SnapError),
-                after=set_retry_status,
-            ):
-                with attempt:
-                    MySQL.install_and_configure_mysql_dependencies()
-        except RetryError:
+        if self.install_workload():
+            self.unit.status = WaitingStatus("Waiting to start MySQL")
+        else:
             self.unit.status = BlockedStatus("Failed to install and configure MySQL")
-            return
-
-        self.unit.status = WaitingStatus("Waiting to start MySQL")
 
     def _on_leader_elected(self, _) -> None:
         """Handle the leader elected event."""
@@ -333,8 +325,9 @@ class MySQLOperatorCharm(MySQLCharmBase):
             and not self.unit_peer_data.get("unit-configured")
             and not self.unit_peer_data.get("unit-initialized")
             and not self.unit.is_leader()
+            and not self.upgrade.idle
         ):
-            # avoid changing status while in initialisation
+            # avoid changing status while in initialising/upgrading
             return
 
         if self._is_unit_waiting_to_join_cluster():
@@ -408,7 +401,7 @@ class MySQLOperatorCharm(MySQLCharmBase):
     def _mysql(self):
         """Returns an instance of the MySQL object."""
         return MySQL(
-            self._get_unit_ip(self.unit),
+            self.get_unit_ip(self.unit),
             self.app_peer_data["cluster-name"],
             self.app_peer_data["cluster-set-domain-name"],
             self.get_secret("app", ROOT_PASSWORD_KEY),
@@ -450,6 +443,30 @@ class MySQLOperatorCharm(MySQLCharmBase):
             return "Primary"
         return ""
 
+    def install_workload(self) -> bool:
+        """Exponential backoff retry to install and configure MySQL.
+
+        Returns: True if successful, False otherwise.
+        """
+
+        def set_retry_status(_):
+            self.unit.status = MaintenanceStatus(
+                "Failed to install and configure MySQL. Retrying..."
+            )
+
+        try:
+            for attempt in Retrying(
+                wait=wait_exponential(multiplier=10),
+                stop=stop_after_delay(60 * 5),
+                retry=tenacity.retry_if_exception_type(snap.SnapError),
+                after=set_retry_status,
+            ):
+                with attempt:
+                    MySQL.install_and_configure_mysql_dependencies()
+        except (RetryError, MySQLInstallError):
+            return False
+        return True
+
     def _workload_initialise(self) -> None:
         """Workload initialisation commands.
 
@@ -466,7 +483,7 @@ class MySQLOperatorCharm(MySQLCharmBase):
         if workload_version := self._mysql.get_mysql_version():
             self.unit.set_workload_version(workload_version)
 
-    def _get_unit_ip(self, unit: Unit) -> Optional[str]:
+    def get_unit_ip(self, unit: Unit) -> str:
         """Get the IP address of a specific unit."""
         if unit == self.unit:
             return str(self.model.get_binding(PEER).network.bind_address)
@@ -504,6 +521,11 @@ class MySQLOperatorCharm(MySQLCharmBase):
         """
         # Safeguard unit starting before leader unit sets peer data
         if not self._is_peer_data_set:
+            event.defer()
+            return False
+
+        # Safeguard against starting while upgrading
+        if not self.upgrade.idle:
             event.defer()
             return False
 
@@ -548,7 +570,7 @@ class MySQLOperatorCharm(MySQLCharmBase):
             # Re-add the member as if it's the first time
 
             self._mysql.add_instance_to_cluster(
-                self._get_unit_ip(self.unit), self.unit_label, from_instance=primary_address
+                self.get_unit_ip(self.unit), self.unit_label, from_instance=primary_address
             )
         except MySQLReconfigureError:
             return MaintenanceStatus("Failed to re-initialize MySQL data-dir")
@@ -598,7 +620,7 @@ class MySQLOperatorCharm(MySQLCharmBase):
             if self.peers.data[unit].get("member-state") == "online":
                 try:
                     return self._mysql.get_cluster_primary_address(
-                        connect_instance_address=self._get_unit_ip(unit)
+                        connect_instance_address=self.get_unit_ip(unit)
                     )
                 except MySQLGetClusterPrimaryAddressError:
                     # try next unit
@@ -610,7 +632,7 @@ class MySQLOperatorCharm(MySQLCharmBase):
         Try to join the unit from the primary unit.
         """
         instance_label = self.unit.name.replace("/", "-")
-        instance_address = self._get_unit_ip(self.unit)
+        instance_address = self.get_unit_ip(self.unit)
 
         if self._mysql.is_instance_in_cluster(instance_label):
             logger.debug("instance already in cluster")
---
+++ src/upgrade.py
@@ -0,0 +1,257 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Dependency model for MySQL."""
+
+import bisect
+import json
+import logging
+from typing import TYPE_CHECKING
+
+from charms.data_platform_libs.v0.upgrade import (
+    ClusterNotReadyError,
+    DataUpgrade,
+    DependencyModel,
+    UpgradeGrantedEvent,
+    VersionError,
+)
+from charms.mysql.v0.mysql import (
+    MySQLGetMySQLVersionError,
+    MySQLServerNotUpgradableError,
+    MySQLSetClusterPrimaryError,
+    MySQLSetVariableError,
+    MySQLStartMySQLDError,
+    MySQLStopMySQLDError,
+)
+from ops.model import BlockedStatus, MaintenanceStatus, Unit
+from pydantic import BaseModel
+from tenacity import RetryError, Retrying, stop_after_attempt, wait_fixed
+from typing_extensions import override
+
+if TYPE_CHECKING:
+    from charm import MySQLOperatorCharm
+
+logger = logging.getLogger(__name__)
+
+
+RECOVER_ATTEMPTS = 10
+
+
+class MySQLVMDependenciesModel(BaseModel):
+    """MySQL dependencies model."""
+
+    charm: DependencyModel
+    snap: DependencyModel
+
+
+def get_mysql_dependencies_model() -> MySQLVMDependenciesModel:
+    """Return the MySQL dependencies model."""
+    with open("src/dependency.json") as dependency_file:
+        _deps = json.load(dependency_file)
+    return MySQLVMDependenciesModel(**_deps)
+
+
+class MySQLVMUpgrade(DataUpgrade):
+    """MySQL upgrade class."""
+
+    def __init__(self, charm: "MySQLOperatorCharm", **kwargs) -> None:
+        """Initialize the class."""
+        super().__init__(charm, **kwargs)
+        self.charm = charm
+        self.framework.observe(
+            self.charm.on[self.relation_name].relation_changed, self._on_upgrade_changed
+        )
+
+    @override
+    def build_upgrade_stack(self) -> list[int]:
+        """Build the upgrade stack.
+
+        This/leader/primary will be the last.
+        Others will be ordered by unit number ascending.
+        Higher unit number will be upgraded first.
+        """
+
+        def unit_number(unit: Unit) -> int:
+            """Return the unit number."""
+            return int(unit.name.split("/")[1])
+
+        upgrade_stack = []
+        for unit in self.peer_relation.units:
+            bisect.insort(upgrade_stack, unit_number(unit))
+
+        upgrade_stack.insert(0, unit_number(self.charm.unit))
+        return upgrade_stack
+
+    @override
+    def pre_upgrade_check(self) -> None:
+        """Run pre-upgrade checks."""
+        fail_message = "Pre-upgrade check failed. Cannot upgrade."
+
+        def _online_instances(status_dict: dict) -> int:
+            """Return the number of online instances from status dict."""
+            return [
+                item["status"]
+                for item in status_dict["defaultreplicaset"]["topology"].values()
+                if not item.get("instanceerrors", [])
+            ].count("online")
+
+        if cluster_status := self.charm._mysql.get_cluster_status(extended=True):
+            if _online_instances(cluster_status) < self.charm.app.planned_units():
+                # case any not fully online unit is found
+                raise ClusterNotReadyError(
+                    message=fail_message,
+                    cause="Not all units are online",
+                    resolution="Ensure all units are online in the cluster",
+                )
+        else:
+            # case cluster status is not available
+            # it may be due to the refresh being ran before
+            # the pre-upgrade-check action
+            raise ClusterNotReadyError(
+                message=fail_message,
+                cause="Failed to retrieve cluster status",
+                resolution="Ensure that mysqld is running for this unit",
+            )
+
+        try:
+            self._pre_upgrade_prepare()
+        except MySQLSetClusterPrimaryError:
+            raise ClusterNotReadyError(
+                message=fail_message,
+                cause="Failed to set primary",
+                resolution="Check the cluster status",
+            )
+        except MySQLSetVariableError:
+            raise ClusterNotReadyError(
+                message=fail_message,
+                cause="Failed to set slow shutdown",
+                resolution="Check the cluster status",
+            )
+
+    @override
+    def _on_upgrade_granted(self, event: UpgradeGrantedEvent) -> None:
+        """Handle the upgrade granted event."""
+        try:
+            self.charm.unit.status = MaintenanceStatus("stopping services..")
+            self.charm._mysql.stop_mysqld()
+
+            self.charm.unit.status = MaintenanceStatus("upgrading snap...")
+            if not self.charm.install_workload():
+                logger.error("Failed to install workload snap")
+                self.set_unit_failed()
+                return
+            self.charm.unit.status = MaintenanceStatus("check if upgrade is possible")
+            self._check_server_upgradeability()
+            self.charm._mysql.start_mysqld()
+        except VersionError:
+            logger.exception("Failed to upgrade MySQL dependencies")
+            self.set_unit_failed()
+            return
+        except (MySQLStopMySQLDError, MySQLStartMySQLDError):
+            logger.exception("Failed to stop/start MySQL server")
+            self.set_unit_failed()
+            return
+
+        try:
+            self.charm.unit.set_workload_version(self.charm._mysql.get_mysql_version() or "unset")
+        except MySQLGetMySQLVersionError:
+            # don't fail on this, just log it
+            logger.warning("Failed to get MySQL version")
+
+        self.charm.unit.status = MaintenanceStatus("recovering unit after upgrade")
+
+        try:
+            for attempt in Retrying(
+                stop=stop_after_attempt(RECOVER_ATTEMPTS), wait=wait_fixed(10)
+            ):
+                with attempt:
+                    if not self.charm._mysql.is_instance_in_cluster(self.charm.unit_label):
+                        logger.debug(
+                            "Instance not yet back in the cluster."
+                            f" Retry {attempt.retry_state.attempt_number}/{RECOVER_ATTEMPTS}"
+                        )
+                        raise Exception
+                    logger.debug("Upgraded unit is healthy. Set upgrade state to `completed`")
+                    self.set_unit_completed()
+                    # ensures leader gets it's own relation-changed when it upgrades
+                    if self.charm.unit.is_leader():
+                        logger.debug("Re-emitting upgrade-changed on leader...")
+                        self.on_upgrade_changed(event)
+        except RetryError:
+            logger.debug("Upgraded unit is not healthy")
+            self.set_unit_failed()
+            self.charm.unit.status = BlockedStatus(
+                "upgrade failed. Check logs for rollback instruction"
+            )
+
+    def _on_upgrade_changed(self, _) -> None:
+        """Handle the upgrade changed event.
+
+        Run update status for every unit when the upgrade is completed.
+        """
+        if not self.upgrade_stack and self.idle:
+            self.charm._on_update_status(None)
+
+    @override
+    def log_rollback_instructions(self) -> None:
+        """Log rollback instructions."""
+        logger.critical(
+            "\n".join(
+                (
+                    "Upgrade failed, follow the instructions below to rollback:",
+                    "    1. Re-run `pre-upgrade-check` action on the leader unit to enter 'recovery' state",
+                    "    2. Run `juju refresh` to the previously deployed charm revision or local charm file",
+                )
+            )
+        )
+
+    def _pre_upgrade_prepare(self) -> None:
+        """Pre upgrade routine for MySQL.
+
+        Set primary to the leader (this) unit to mitigate switchover during upgrade,
+        and set slow shutdown to all instances.
+        """
+        if self.charm._mysql.get_primary_label() != self.charm.unit_label:
+            # set the primary to the leader unit for switchover mitigation
+            self.charm._mysql.set_cluster_primary(self.charm.get_unit_ip(self.charm.unit))
+
+        # set slow shutdown on all instances
+        for unit in self.app_units:
+            unit_address = self.charm.get_unit_ip(unit)
+            self.charm._mysql.set_dynamic_variable(
+                variable="innodb_fast_shutdown", value="0", instance_address=unit_address
+            )
+
+    def _check_server_upgradeability(self) -> None:
+        """Check if the server can be upgraded.
+
+        Use mysql-shell upgrade checker utility to ensure server upgradeability.
+
+        Raises:
+            VersionError: If the server is not upgradeable.
+        """
+        if len(self.upgrade_stack or []) < self.charm.app.planned_units():
+            # check is done for first upgrading unit only
+            return
+
+        def leader_unit_address() -> str:
+            # Return the leader unit address.
+            # leader is update stack first item
+            leader_unit_ordinal = self.upgrade_stack[0]
+            for unit in self.peer_relation.units:
+                if unit.name == f"{self.charm.app.name}/{leader_unit_ordinal}":
+                    return self.charm.get_unit_ip(unit)
+            return ""
+
+        try:
+            # verifies if the server is upgradeable by connecting to the leader
+            # which is running the pre-upgraded mysql-server version
+            self.charm._mysql.verify_server_upgradable(instance=leader_unit_address())
+            logger.debug("MySQL server is upgradeable")
+        except MySQLServerNotUpgradableError as e:
+            logger.error("MySQL server is not upgradeable")
+            raise VersionError(
+                message="Cannot upgrade MySQL server",
+                cause=e.message,
+                resolution="Check mysql-shell upgrade utility output for more details",
+            )
