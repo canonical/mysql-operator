5c9828679b1b859a51c893899a9fc3caeb8d05f0
Paulo Machado <paulo.machado@canonical.com>
2023-09-01 21:51:47 -0300

    split upgrade from stable

--- tests/integration/high_availability/test_upgrade.py
+++ tests/integration/high_availability/test_upgrade.py
@@ -31,7 +31,7 @@ MYSQL_APP_NAME = "mysql"
 TEST_APP_NAME = "mysql-test-app"
 
 
-@pytest.mark.group(2)
+@pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_deploy_latest(ops_test: OpsTest, mysql_charm_series: str) -> None:
     """Simple test to ensure that the mysql and application charms get deployed."""
@@ -61,7 +61,7 @@ async def test_deploy_latest(ops_test: OpsTest, mysql_charm_series: str) -> None
     assert len(ops_test.model.applications[MYSQL_APP_NAME].units) == 3
 
 
-@pytest.mark.group(2)
+@pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_pre_upgrade_check(ops_test: OpsTest) -> None:
     """Test that the pre-upgrade-check action runs successfully."""
@@ -86,7 +86,7 @@ async def test_pre_upgrade_check(ops_test: OpsTest) -> None:
     assert await primary_unit.is_leader_from_status(), "Primary unit not set to leader"
 
 
-@pytest.mark.group(2)
+@pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_upgrade_from_edge(
     ops_test: OpsTest,
@@ -119,7 +119,7 @@ async def test_upgrade_from_edge(
     await ensure_all_units_continuous_writes_incrementing(ops_test)
 
 
-@pytest.mark.group(2)
+@pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_fail_and_rollback(ops_test, continuous_writes) -> None:
     logger.info("Get leader unit")
@@ -169,47 +169,6 @@ async def test_fail_and_rollback(ops_test, continuous_writes) -> None:
 
     # remove fault charm file
     os.remove(fault_charm)
-
-
-@pytest.mark.group(2)
-@pytest.mark.abort_on_fail
-async def test_upgrade_from_stable(ops_test: OpsTest, mysql_charm_series: str):
-    """Test updating from stable channel."""
-    logger.info("Remove mysql")
-    await ops_test.model.remove_application(MYSQL_APP_NAME, block_until_done=True)
-    logger.info("Deploy mysql from stable")
-    await ops_test.model.deploy(
-        MYSQL_APP_NAME,
-        application_name=MYSQL_APP_NAME,
-        num_units=3,
-        channel="8.0/stable",
-        series=mysql_charm_series,
-        constraints="mem=2G",
-        # config={"profile": "testing"}, # config not available in 8.0/stable@r151
-    )
-    logger.info("Relate test application")
-    await relate_mysql_and_application(ops_test, MYSQL_APP_NAME, TEST_APP_NAME)
-
-    application = ops_test.model.applications[MYSQL_APP_NAME]
-    logger.info("Build charm locally")
-    charm = await ops_test.build_charm(".")
-
-    logger.info("Refresh the charm")
-    await application.refresh(path=charm)
-
-    logger.info("Wait for upgrade to start")
-    await ops_test.model.block_until(
-        lambda: "maintenance" in {unit.workload_status for unit in application.units},
-        timeout=TIMEOUT,
-    )
-
-    logger.info("Wait for upgrade to complete")
-    await ops_test.model.wait_for_idle(
-        apps=[MYSQL_APP_NAME], status="active", idle_period=30, timeout=TIMEOUT
-    )
-
-    logger.info("Ensure continuous_writes")
-    await ensure_all_units_continuous_writes_incrementing(ops_test)
 
 
 async def inject_dependency_fault(
