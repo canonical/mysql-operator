2a98e6eedb76a4a61d9cb811c0342a21ae52e2f4
Carl Csaposs <carl.csaposs@canonical.com>
2023-11-16 13:34:56 +0000

    Pass path to s3 integrator config

--- tests/integration/test_backups.py
+++ tests/integration/test_backups.py
@@ -43,11 +43,14 @@ value_before_backup, value_after_backup = None, None
 
 
 @pytest.fixture(scope="session")
-def cloud_configs():
+def path():
     # Add UUID to path to avoid conflict with tests running in parallel (e.g. multiple Juju
     # versions on a PR, multiple PRs)
-    path = f"mysql-{uuid.uuid4()}"
+    return f"mysql-{uuid.uuid4()}"
 
+
+@pytest.fixture(scope="session")
+def cloud_configs(path):
     return {
         "aws": {
             "endpoint": "https://s3.amazonaws.com",
@@ -106,7 +109,7 @@ def clean_backups_from_buckets(cloud_configs, cloud_credentials) -> None:
 
 
 @pytest.mark.group(1)
-async def test_build_and_deploy(ops_test: OpsTest, mysql_charm_series: str) -> None:
+async def test_build_and_deploy(ops_test: OpsTest, mysql_charm_series: str, path) -> None:
     """Simple test to ensure that the mysql charm gets deployed."""
     mysql_application_name = await deploy_and_scale_mysql(ops_test, mysql_charm_series)
 
@@ -124,7 +127,9 @@ async def test_build_and_deploy(ops_test: OpsTest, mysql_charm_series: str) -> N
 
     logger.info("Deploying s3 integrator")
 
-    await ops_test.model.deploy(S3_INTEGRATOR, channel=S3_INTEGRATOR_CHANNEL)
+    await ops_test.model.deploy(
+        S3_INTEGRATOR, channel=S3_INTEGRATOR_CHANNEL, config={"path": path}
+    )
     await ops_test.model.relate(mysql_application_name, S3_INTEGRATOR)
 
     await ops_test.model.wait_for_idle(
