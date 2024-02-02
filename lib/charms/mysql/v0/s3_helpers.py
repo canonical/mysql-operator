e306b1926531bd4b2f6f8ce37118d6cb5805ecac
Peter Sabaini <peter.sabaini@canonical.com>
2024-02-02 10:43:13 +0100

    Add support for s3 tls-ca-chain
    Fixes #395
    
    Signed-off-by: Peter Sabaini <peter.sabaini@canonical.com>

--- lib/charms/mysql/v0/s3_helpers.py
+++ lib/charms/mysql/v0/s3_helpers.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 """S3 helper functions for the MySQL charms."""
-
+import base64
 import logging
 import tempfile
 import time
@@ -57,8 +57,20 @@ def upload_content_to_s3(content: str, content_path: str, s3_parameters: Dict) -
             aws_secret_access_key=s3_parameters["secret-key"],
             region_name=s3_parameters["region"] or None,
         )
+        verif = True
+        ca_chain = s3_parameters["tls-ca-chain"]
+        if ca_chain:
+            ca = "\n".join([base64.b64decode(s).decode() for s in ca_chain])
+            ca_file = tempfile.NamedTemporaryFile()
+            ca_file.write(ca.encode())
+            ca_file.flush()
+            verif = ca_file.name
 
-        s3 = session.resource("s3", endpoint_url=s3_parameters["endpoint"])
+        s3 = session.resource(
+            "s3",
+            endpoint_url=s3_parameters["endpoint"],
+            verify=verif,
+        )
 
         bucket = s3.Bucket(s3_parameters["bucket"])
 
