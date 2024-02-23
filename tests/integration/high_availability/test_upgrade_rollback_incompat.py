3f5d6325b2fdf608f3bb4200a85de63545cab225
Paulo Machado <paulo.machado@canonical.com>
2024-02-23 16:31:50 -0300

    sed/git instead of diff/patch

--- tests/integration/high_availability/test_upgrade_rollback_incompat.py
+++ tests/integration/high_availability/test_upgrade_rollback_incompat.py
@@ -4,7 +4,6 @@
 import ast
 import logging
 import subprocess
-from tempfile import NamedTemporaryFile
 
 import pytest
 from pytest_operator.plugin import OpsTest
@@ -20,49 +19,21 @@ logger = logging.getLogger(__name__)
 
 TIMEOUT = 20 * 60
 MYSQL_APP_NAME = "mysql"
-PATCH_OLD_SNAP = (
-    "diff --git a/src/constants.py b/src/constants.py\n"
-    "index 4d6078e..515cf41 100644\n"
-    "--- a/src/constants.py\n"
-    "+++ b/src/constants.py\n"
-    '@@ -25,7 +25,7 @@ TLS_SSL_KEY_FILE = "custom-server-key.pem"\n'
-    ' TLS_SSL_CERT_FILE = "custom-server-cert.pem"\n'
-    " MYSQL_EXPORTER_PORT = 9104\n"
-    ' CHARMED_MYSQL_SNAP_NAME = "charmed-mysql"\n'
-    "-CHARMED_MYSQL_SNAP_REVISION = 96  # MySQL v8.0.35\n"
-    "+CHARMED_MYSQL_SNAP_REVISION = 69  # MySQL v8.0.34\n"
-    ' CHARMED_MYSQLD_EXPORTER_SERVICE = "mysqld-exporter"\n'
-    ' CHARMED_MYSQLD_SERVICE = "mysqld"\n'
-    ' CHARMED_MYSQL = "charmed-mysql.mysql"\n'
-)
-PATCH_FAIL_UPGRADE = (
-    "diff --git a/src/upgrade.py b/src/upgrade.py\n"
-    "index 5b9c861..41b85fd 100644\n"
-    "--- a/src/upgrade.py\n"
-    "+++ b/src/upgrade.py\n"
-    "@@ -240,6 +240,7 @@ class MySQLVMUpgrade(DataUpgrade):\n"
-    " \n"
-    "     def _recover_multi_unit_cluster(self) -> None:\n"
-    '         logger.debug("Recovering unit")\n'
-    "+        self.charm._mysql.set_instance_offline_mode(True)\n"
-    "         try:\n"
-    "             for attempt in Retrying(\n"
-    "                 stop=stop_after_attempt(RECOVER_ATTEMPTS), wait=wait_fixed(10)\n"
-)
 
 
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest, mysql_charm_series: str) -> None:
     """Simple test to ensure that the mysql and application charms get deployed."""
-    src_patch(patch=PATCH_OLD_SNAP)
+    sub_regex_older_snap = "s/CHARMED_MYSQL_SNAP_REVISION.*/CHARMED_MYSQL_SNAP_REVISION = 69/"
+    src_patch(sub_regex=sub_regex_older_snap, file_name="src/constants.py")
     # store for later refreshing to it
     global charm
     charm = await ops_test.build_charm(".")
 
     await high_availability_test_setup(ops_test, mysql_charm_series)
 
-    src_patch(patch=PATCH_OLD_SNAP, revert=True)
+    src_patch(revert=True)
 
 
 @pytest.mark.group(1)
@@ -89,9 +60,12 @@ async def test_upgrade_to_failling(
     application = ops_test.model.applications[MYSQL_APP_NAME]
     logger.info("Build charm locally")
 
-    src_patch(patch=PATCH_FAIL_UPGRADE)
+    sub_regex_failing_rejoin = (
+        's/logger.debug("Recovering unit")/self.charm._mysql.set_instance_offline_mode(True)/'
+    )
+    src_patch(sub_regex=sub_regex_failing_rejoin, file_name="src/upgrade.py")
     new_charm = await ops_test.build_charm(".")
-    src_patch(patch=PATCH_FAIL_UPGRADE, revert=True)
+    src_patch(revert=True)
 
     logger.info("Refresh the charm")
     await application.refresh(path=new_charm)
@@ -144,12 +118,10 @@ async def test_rollback(ops_test, continuous_writes) -> None:
     await ensure_all_units_continuous_writes_incrementing(ops_test)
 
 
-def src_patch(patch: str, revert: bool = False) -> None:
+def src_patch(sub_regex: str = "", file_name: str = "", revert: bool = False) -> None:
     """Apply a patch to the source code."""
-    with NamedTemporaryFile("w", delete=False) as patch_file:
-        patch_file.write(patch)
-        if revert:
-            cmd = f"patch -R -p1 <{patch_file.name}"
-        else:
-            cmd = f"patch -p1 <{patch_file.name}"
-        subprocess.run([cmd], shell=True, check=True)
+    if revert:
+        cmd = "git checkout ."  # revert all changes
+    else:
+        cmd = f"sed -i -e '{sub_regex}' {file_name}"
+    subprocess.run([cmd], shell=True, check=True)
