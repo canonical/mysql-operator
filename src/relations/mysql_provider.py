a6d20471b5742435f8a8d72b11e5aee33f480b3b
Paulo Machado <paulo.machado@canonical.com>
2024-07-03 09:59:19 -0300

    model uuid as suffix

--- src/relations/mysql_provider.py
+++ src/relations/mysql_provider.py
@@ -206,7 +206,7 @@ class MySQLProvider(Object):
         """
         if legacy:
             return f"relation-{relation_id}"
-        return f"{self.model.uuid.replace('-', '')}_relation-{relation_id}"[-32:]
+        return f"relation-{relation_id}_{self.model.uuid.replace('-', '')}"[:32]
 
     def _on_database_requested(self, event: DatabaseRequestedEvent):
         """Handle the `database-requested` event."""
