90bb1fbce454ae8e89a8e4d1d28de9453ee932d8
Paulo Machado <paulo.machado@canonical.com>
2024-07-05 10:35:59 -0300

    DPE-4643 ensure username uniqueness  (#464)
    * ensure username uniqueness
    
    * libs bump
    
    * temporary branch for router
    
    * test against temp branch revision of router
    
    * support for legacy username format
    
    * model uuid as suffix
    
    * trunc at 26chars
    
    * leftover

--- src/relations/mysql_provider.py
+++ src/relations/mysql_provider.py
@@ -194,6 +194,20 @@ class MySQLProvider(Object):
         self.database.update_relation_data(relation.id, {"password": password})
         return password
 
+    def _get_username(self, relation_id: int, legacy: bool = False) -> str:
+        """Generate a unique username for the relation using the model uuid and the relation id.
+
+        Args:
+            relation_id (int): The relation id.
+            legacy (bool): If True, generate a username without the model uuid.
+
+        Returns:
+            str: A valid unique username (max 32 characters long)
+        """
+        if legacy:
+            return f"relation-{relation_id}"
+        return f"relation-{relation_id}_{self.model.uuid.replace('-', '')}"[:26]
+
     def _on_database_requested(self, event: DatabaseRequestedEvent):
         """Handle the `database-requested` event."""
         if not self.charm.unit.is_leader():
@@ -211,7 +225,7 @@ class MySQLProvider(Object):
         if event.extra_user_roles:
             extra_user_roles = event.extra_user_roles.split(",")
         # user name is derived from the relation id
-        db_user = f"relation-{relation_id}"
+        db_user = self._get_username(relation_id)
         db_pass = self._get_or_set_password(event.relation)
 
         remote_app = event.app.name
@@ -274,8 +288,18 @@ class MySQLProvider(Object):
 
         relation_id = event.relation.id
         try:
-            self.charm._mysql.delete_users_for_relation(relation_id)
-            logger.info(f"Removed user for relation {relation_id}")
+            if self.charm._mysql.does_mysql_user_exist(self._get_username(relation_id), "%"):
+                self.charm._mysql.delete_users_for_relation(self._get_username(relation_id))
+            elif self.charm._mysql.does_mysql_user_exist(
+                self._get_username(relation_id, legacy=True), "%"
+            ):
+                self.charm._mysql.delete_users_for_relation(
+                    self._get_username(relation_id, legacy=True)
+                )
+            else:
+                logger.warning(f"User(s) not found for relation {relation_id}")
+                return
+            logger.info(f"Removed user(s) for relation {relation_id}")
         except (MySQLDeleteUsersForRelationError, KeyError):
             logger.error(f"Failed to delete user for relation {relation_id}")
 
