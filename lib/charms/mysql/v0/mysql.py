e0d23e8b07663fb33f589a4896d88acd9b074194
Shayan Patel <shayan.patel@canonical.com>
2024-11-26 20:24:16 +0000

    Loosen the manual server shell compatibility check to allow for shell 8.0.38 built for 8.0.39

--- lib/charms/mysql/v0/mysql.py
+++ lib/charms/mysql/v0/mysql.py
@@ -134,7 +134,7 @@ LIBID = "8c1428f06b1b4ec8bf98b7d980a38a8c"
 # Increment this major API version when introducing breaking changes
 LIBAPI = 0
 
-LIBPATCH = 76
+LIBPATCH = 77
 
 UNIT_TEARDOWN_LOCKNAME = "unit-teardown"
 UNIT_ADD_LOCKNAME = "unit-add"
@@ -2311,7 +2311,7 @@ class MySQLBase(ABC):
             "try:",
             "    util.check_for_server_upgrade(options={'outputFormat': 'JSON'})",
             "except ValueError:",  # ValueError is raised for same version check
-            "    if session.run_sql('select @@version').fetch_all()[0][0].split('-')[0] == shell.version.split()[1]:",
+            "    if session.run_sql('select @@version').fetch_all()[0][0].split('-')[0] in shell.version:",
             "        print('SAME_VERSION')",
             "    else:",
             "        raise",
