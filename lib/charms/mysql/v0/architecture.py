9ec8471ffbb7bfb2635c2679535faf106b4f5e94
Sinclert Pérez <sinclert.perez@canonical.com>
2024-12-09 10:12:39 +0100

    Create initial architecture lib (II)

--- lib/charms/mysql/v0/architecture.py
+++ lib/charms/mysql/v0/architecture.py
@@ -31,6 +31,8 @@ if __name__ == "__main__":
 
 import logging
 import os
+import pathlib
+import platform
 import sys
 
 from ops import BlockedStatus, CharmBase
@@ -52,27 +54,27 @@ class WrongArchitectureWarningCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
 
-        hw_arch = os.uname().machine
+        hw_arch = platform.machine()
         self.unit.status = BlockedStatus(f"Error: Charm incompatible with {hw_arch} architecture")
         sys.exit(0)
 
 
 def is_wrong_architecture() -> bool:
     """Checks if charm was deployed on wrong architecture."""
-    manifest_path = f"{os.environ.get('CHARM_DIR')}/manifest.yaml"
+    manifest_path = pathlib.Path(os.environ["CHARM_DIR"], "manifest.yaml")
 
-    if not os.path.exists(manifest_path):
+    if not manifest_path.exists():
         logger.error("Cannot check architecture: manifest file not found in %s", manifest_path)
         return False
 
     with open(manifest_path, "r") as file:
         manifest = file.read()
 
-    hw_arch = os.uname().machine
+    hw_arch = platform.machine()
     if ("amd64" in manifest and hw_arch == "x86_64") or (
         "arm64" in manifest and hw_arch == "aarch64"
     ):
-        logger.info("Charm architecture matches")
+        logger.debug("Charm architecture matches")
         return False
 
     logger.error("Charm architecture does not match")
