714af06131d9957686c8b2e2acd432d635a5576b
Sinclert Pérez <sinclert.perez@canonical.com>
2024-12-09 10:42:59 +0100

    Create initial architecture unit tests (II)

--- tests/unit/test_architecture.py
+++ tests/unit/test_architecture.py
@@ -11,8 +11,8 @@ from charms.mysql.v0.architecture import is_wrong_architecture
 def test_wrong_architecture_file_not_found():
     """Tests if the function returns False when the charm file doesn't exist."""
     with (
-        patch("os.environ.get", return_value="/tmp"),
-        patch("os.path.exists", return_value=False),
+        patch("os.environ", return_value={"CHARM_DIR": "/tmp"}),
+        patch("pathlib.Path.exists", return_value=False),
     ):
         assert not is_wrong_architecture()
 
@@ -20,26 +20,26 @@ def test_wrong_architecture_file_not_found():
 def test_wrong_architecture_amd64():
     """Tests if the function correctly identifies arch when charm is AMD."""
     with (
-        patch("os.environ.get", return_value="/tmp"),
-        patch("os.path.exists", return_value=True),
+        patch("os.environ", return_value={"CHARM_DIR": "/tmp"}),
+        patch("pathlib.Path.exists", return_value=True),
         patch("builtins.open", mock.mock_open(read_data="amd64\n")),
-        patch("os.uname") as _uname,
+        patch("platform.machine") as machine,
     ):
-        _uname.return_value = mock.Mock(machine="x86_64")
+        machine.return_value = "x86_64"
         assert not is_wrong_architecture()
-        _uname.return_value = mock.Mock(machine="aarch64")
+        machine.return_value = "aarch64"
         assert is_wrong_architecture()
 
 
 def test_wrong_architecture_arm64():
     """Tests if the function correctly identifies arch when charm is ARM."""
     with (
-        patch("os.environ.get", return_value="/tmp"),
-        patch("os.path.exists", return_value=True),
+        patch("os.environ", return_value={"CHARM_DIR": "/tmp"}),
+        patch("pathlib.Path.exists", return_value=True),
         patch("builtins.open", mock.mock_open(read_data="arm64\n")),
-        patch("os.uname") as _uname,
+        patch("platform.machine") as machine,
     ):
-        _uname.return_value = mock.Mock(machine="x86_64")
+        machine.return_value = "x86_64"
         assert is_wrong_architecture()
-        _uname.return_value = mock.Mock(machine="aarch64")
+        machine.return_value = "aarch64"
         assert not is_wrong_architecture()
