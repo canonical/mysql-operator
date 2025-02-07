31a64962e1f2dc3ba54cfa6aceecc28698c33709
Paulo Machado <paulo.machado@canonical.com>
2025-02-07 16:24:07 -0300

    fix sed string

--- tests/integration/high_availability/test_upgrade_rollback_incompat.py
+++ tests/integration/high_availability/test_upgrade_rollback_incompat.py
@@ -110,7 +110,7 @@ async def test_upgrade_to_failling(
 
     sub_regex_failing_rejoin = (
         's/logger.info("Recovering unit")'
-        "/self._mysql.set_instance_offline_mode(True); raise RetryError('dummy')/"
+        '/self._mysql.set_instance_offline_mode(True); raise RetryError("dummy")/'
     )
     src_patch(sub_regex=sub_regex_failing_rejoin, file_name="src/charm.py")
     new_charm = await charm_local_build(ops_test, refresh=True)
