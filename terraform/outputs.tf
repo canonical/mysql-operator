6368778daf97941f9496e0f128920439c5da7432
Sinclert Pérez <sinclert.perez@canonical.com>
2025-09-29 12:41:07 +0200

    Tweak terraform module

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,10 @@
-output "application_name" {
-  value = juju_application.machine_mysql.name
+output "app_name" {
+  description = "Name of the MySQL Server VM application"
+  value       = juju_application.mysql_server.name
 }
 
 output "provides" {
+  description = "Map of all the provided endpoints"
   value = {
     database  = "database",
     cos_agent = "cos-agent",
@@ -10,8 +12,10 @@ output "provides" {
 }
 
 output "requires" {
+  description = "Map of all the required endpoints"
   value = {
     certificates  = "certificates"
     s3_parameters = "s3-parameters"
+    tracing       = "tracing"
   }
 }
